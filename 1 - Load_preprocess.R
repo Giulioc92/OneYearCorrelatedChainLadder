2c9d563885c6d5b5c06e3d9bef593555a4a04dce
Giulio Ercole Carnevale <giulio.ercole.carnevale@gmail.com>
2019-05-26 00:32:52 +0200

    pre process update

--- 1 - Load_preprocess.R
+++ 1 - Load_preprocess.R
@@ -63,7 +63,27 @@ mack_lr$to_pay <- 1 - mack_lr$dev_to_date
 mack_lr$lr_paid <- mack_lr$latest / unique(data$premiums)
 mack_lr
 
-  #### benchmark to mix with prior information about loss ratio
+#### fit a log normal distribution on the loss ratio
+#### to get a reference point on the loss ratio parameter space
+
+lr_dist_param <- fitdistrplus::fitdist(mack_lr$lr,distr = 'lnorm',method = 'mle')
+exp(lr_dist_param[[1]][1]+lr_dist_param[[1]][1]**2/2) 
+sigma_emp <- lr_dist_param[[1]][2]
+#### the hypothesis for the prior loss ratio is log normal,
+#### I'd like to know, for a given loss ratio mean and a sigma, the mu parameters
+#### that could be used to generate the logarithm of this loss ratio from a normal distribution,
+#### or to generate sample from a log normal distribution centered on a particular value
+#### (assuming a fixed sigma, estimate the mu of the lognormal that would imply the Mack ultimate costs)
+find_the_mu <- function(mean,sigma, lognormal = T){
+  if(lognormal == T){
+  mu = log(mean) - sigma**2/2
+  } else {
+  mu = mean - sigma**2/2  
+  }
+return(mu)
+  }
+
+#### benchmark to mix with prior information about loss ratio
 
 #### A normal distribution is chosen for prior loss ratio
 #### I decide to set a mean value for the prior loss ratio,based on my evaluations
@@ -73,7 +93,7 @@ mack_lr
 
 (mean_lossratio <- mack_lr$lr %>% mean)
 
-lr_priors <- mack_lr  %>% mutate(ibnr = ultimate - latest,lr_to_be_paid = (lr-lr_paid)/lr) %>% 
+lr_priors <- mack_lr %>% mutate(ibnr = ultimate - latest,lr_to_be_paid = (lr-lr_paid)/lr) %>% 
   select(ultimate,latest,ibnr,lr_paid,lr,lr_to_be_paid)
 lr_priors$prior_lr <- 0 
   
@@ -84,17 +104,17 @@ lr_priors[3,7] <- lr_priors[3,5]
 #### external knowledge
 #### the more is left to pay the more we make room for uncertainty
 
-lr_priors[4,7] <- lr_priors[4,5] * (1.03)
-lr_priors[5,7] <- lr_priors[5,5] * (1.03)
-lr_priors[6,7] <- lr_priors[6,5] * (1.02)
-lr_priors[7,7] <- lr_priors[7,5] * (1.02)
-lr_priors[8,7] <- lr_priors[8,5] * (1.02)
-lr_priors[9,7] <- lr_priors[9,5] * (1.02)
+lr_priors[4,7] <- .731
+lr_priors[5,7] <- .806
+lr_priors[6,7] <- .861  ### lr_priors[6,5] * (1.02)
+lr_priors[7,7] <- .695  ### lr_priors[7,5] * (1.02)
+lr_priors[8,7] <- .807
+lr_priors[9,7] <- .7036
 
 #### particular care for the two youngest generations
 #### implied loss ratio are lower than long-term loss ratio of the portfolio
 #### I converge to that value
-lr_priors[10,7] <- mean_lossratio * 1.02
+lr_priors[10,7] <- .7428
 
 ### the last loss ratio in particular seems not to
 ### be in line with that ot immediately preceding generations:
@@ -109,15 +129,24 @@ lr_priors %>% mutate(d_to_d = mack_lr$dev_to_date,prior_ult = prior_lr*premiums,
                                'prior > lat' = prior_ult > latest) %>% 
   select(latest,ultimate,d_to_d, lr, prior_ult,prior_lr,'prior > lat')
 
-### log-transform to feed the MCMC sampler
+### get the parameters for a log-normal loss ratio to feed the MCMC sampler
 
-log_priors <- lr_priors %>% mutate(log_prior = log(prior_lr),log_mack_lr = log(lr)) %>% 
-  select(log_mack_lr,log_prior,lr_to_be_paid)
+sapply(lr_priors$prior_lr,function(p) find_the_mu(p,sigma_emp)) %>% unname
+
+log_priors <- lr_priors %>% mutate(log_mu_prior = find_the_mu(prior_lr,sigma_emp),
+                                   log_mack_lr = find_the_mu(lr,sigma_emp),to_be_perc = (lr_to_be_paid)/(lr_to_be_paid + lr_paid)) %>% 
+  select(lr,log_mack_lr,log_mu_prior,lr_paid,lr_to_be_paid,to_be_perc,prior_lr)
 
 ### check lognormality assumptions
+# old
+# mack_est <- MackChainLadder(tri, est.sigma = 'Mack')
 
-mack_est <- MackChainLadder(tri, est.sigma = 'Mack')
-  mack_est$FullTriangle %>% apply(2,function(p) fitdistrplus::fitdist(p,distr = 'lnorm',method = 'mle'))
+# mle_param <- mack_est$FullTriangle %>% apply(2,function(p) fitdistrplus::fitdist(p,distr = 'lnorm',method = 'mle'))
+# last_sd <- mle_param[[11]]$estimate[2]
+
+
+##### I got reference points on the mu parameter space
+  
 
 ### check variability of log-ultimate cost in data
 ### look at data to assess a reasonable domain of variation
