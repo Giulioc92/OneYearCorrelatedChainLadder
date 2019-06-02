81012f0d798ba24d03ecd08df1472dabc73ec4fd
Giulio Ercole Carnevale <giulio.ercole.carnevale@gmail.com>
2019-06-02 23:26:49 +0200

    Inference/training

--- 2 - Inference.R
+++ 2 - Inference.R
@@ -3,25 +3,26 @@
 
 ### to change the extract in fit_fast
 
-my_seed = 190758
+#my_seed_old = 190758
+my_seed = 1462019
 tic()
 ########### fit the stan model
-ccl_posterior <- stan_model(file = 'ccl_normal_loglossratio_3.stan')
+ccl_posterior <- stan_model(file = 'ccl_lognormal_lossratio6.stan')
 ########### get 10000 posterior sample  ### called here fit_fast for convenience
 
-fit <- sampling(ccl_posterior, data = stan_data, seed = my_seed, warmup = 5000, init = 'random', iter = 30000,
-                thin = 10, chains = 4, cores = 3, control = list(adapt_delta=.999, max_treedepth = 10))
+fit <- sampling(ccl_posterior, data = stan_data, seed = my_seed, warmup = 2500, init = 'random', iter = 27500,
+                thin = 10, chains = 4, cores = 3, control = list(adapt_delta=.98, max_treedepth = 13))
 
 print(fit, pars = c('mu','r_alpha','r_beta', 'a', 'lp__'), include = F)
 toc()
 print(fit, pars = 'logloss', include = F)
 
-###############
-my_seed = 123456
+############### 2802
+my_seed = 1462019
 tic()
-ccl_posterior <- stan_model(file = 'ccl_normal_loglossratio_3.stan')
+ccl_posterior <- stan_model(file = 'ccl_lognormal_lossratio6.stan')
 fit <- sampling(ccl_posterior, data = stan_data, seed = my_seed, warmup = 1000, init = "random", iter = 6000,
-                     thin = 2, chains = 4, cores = 3, control = list(adapt_delta=.9, max_treedepth = 12))
+                     thin = 2, chains = 1, cores = 3, control = list(adapt_delta=.9, max_treedepth = 12))
 print(fit, pars = c('mu', 'a', 'lp__'), include = F)
 toc()
 ###############
@@ -29,6 +30,7 @@ traceplot(fit, 'alpha')
 traceplot(fit, 'beta')
 traceplot(fit, 'sig2')
 traceplot(fit, 'rho') + scale_color_discrete()
+traceplot(fit,'elr')
 ###################
 ### By looking at the both the R_hat, effective sample size and traceplot 
 ### Where are confident to have a strong sample, truly representative of
