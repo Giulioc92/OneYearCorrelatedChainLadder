539d8d740511af50592cc370a4f7e91bd458bcb2
Giulio Ercole Carnevale <giulio.ercole.carnevale@gmail.com>
2019-06-02 23:26:26 +0200

    Pre_processing updated

--- 1 - Load_preprocess.R
+++ 1 - Load_preprocess.R
@@ -69,20 +69,27 @@ mack_lr
 lr_dist_param <- fitdistrplus::fitdist(mack_lr$lr,distr = 'lnorm',method = 'mle')
 exp(lr_dist_param[[1]][1]+lr_dist_param[[1]][1]**2/2) 
 sigma_emp <- lr_dist_param[[1]][2]
+
 #### the hypothesis for the prior loss ratio is log normal,
 #### I'd like to know, for a given loss ratio mean and a sigma, the mu parameters
 #### that could be used to generate the logarithm of this loss ratio from a normal distribution,
 #### or to generate sample from a log normal distribution centered on a particular value
 #### (assuming a fixed sigma, estimate the mu of the lognormal that would imply the Mack ultimate costs)
+
 find_the_mu <- function(mean,sigma, lognormal = T){
   if(lognormal == T){
-  mu = log(mean) - sigma**2/2
+  mu = log(mean) - (sigma**2)/2
   } else {
-  mu = mean - sigma**2/2  
+  mu = mean - (sigma**2)/2  
   }
 return(mu)
   }
 
+### get the mean when sigma to be squared
+ln_mean <- function(mu,sigma){
+  mean = exp(mu + (sigma**2)/2)
+  return(mean)
+}
 #### benchmark to mix with prior information about loss ratio
 
 #### A normal distribution is chosen for prior loss ratio
@@ -99,6 +106,7 @@ lr_priors$prior_lr <- 0
   
 lr_priors[2,7] <- lr_priors[2,5]
 lr_priors[3,7] <- lr_priors[3,5]
+
 #### untouched, we trust Mack, not real variation on the final loss ratio
 #### The idea is to trust less and less Mack implied loss ratio and more our
 #### external knowledge
@@ -106,15 +114,16 @@ lr_priors[3,7] <- lr_priors[3,5]
 
 lr_priors[4,7] <- .731
 lr_priors[5,7] <- .806
-lr_priors[6,7] <- .861  ### lr_priors[6,5] * (1.02)
-lr_priors[7,7] <- .695  ### lr_priors[7,5] * (1.02)
-lr_priors[8,7] <- .807
-lr_priors[9,7] <- .7036
+lr_priors[6,7] <- .851  ### lr_priors[6,5] * (1.02)
+lr_priors[7,7] <- .685  ### lr_priors[7,5] * (1.02)
+lr_priors[8,7] <- .787
+lr_priors[9,7] <- .705
 
 #### particular care for the two youngest generations
 #### implied loss ratio are lower than long-term loss ratio of the portfolio
 #### I converge to that value
-lr_priors[10,7] <- .7428
+
+lr_priors[10,7] <- .743
 
 ### the last loss ratio in particular seems not to
 ### be in line with that ot immediately preceding generations:
@@ -129,6 +138,8 @@ lr_priors %>% mutate(d_to_d = mack_lr$dev_to_date,prior_ult = prior_lr*premiums,
                                'prior > lat' = prior_ult > latest) %>% 
   select(latest,ultimate,d_to_d, lr, prior_ult,prior_lr,'prior > lat')
 
+### and check that the final loss ratio is higher than the paid loss-ratio
+### (assumption of no more recoverables)
 ### get the parameters for a log-normal loss ratio to feed the MCMC sampler
 
 sapply(lr_priors$prior_lr,function(p) find_the_mu(p,sigma_emp)) %>% unname
@@ -150,7 +161,8 @@ log_priors <- lr_priors %>% mutate(log_mu_prior = find_the_mu(prior_lr,sigma_emp
 
 ### check variability of log-ultimate cost in data
 ### look at data to assess a reasonable domain of variation
+
 alpha_start <- data.frame(log_prem = unique(data$premiums) %>% log )
 alpha_start$log_ult <- mack_lr %>% select(log_ult_mack = ultimate) %>% log
-alpha_start$log_ult_prior <- alpha_start$log_prem + log_priors$log_prior
+alpha_start$log_ult_prior <- alpha_start$log_prem + log_priors$log_mu_prior
 alpha_start$diff <- alpha_start[,3] - alpha_start[,1]
